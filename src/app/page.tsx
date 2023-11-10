f17301851b5c7e536bc2fa4ba3bc0bf8e8509129
Nachiket Pusalkar <33430835+nachiket87@users.noreply.github.com>
2023-11-10 16:51:29 -0500

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,83 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import SimpleCard from '@/ui/simplecard';
+import { demos } from '@/lib/demo';
 
 export default function Home() {
   return (
     <>
-      <SimpleCard>
-        <div className="grid gap-x-4 grid-cols-4 max-sm:grid-cols-1 justify-center">
-          <div className="col-span-1 max-sm:flex max-sm:justify-center">
-            <Image
-              className="rounded-full"
-              src="/photo.webp"
-              alt="profile picture"
-              priority
-              width={160}
-              height={160}
-              style={{ width: 'auto', height: 'auto' }}
-            />
-          </div>
-          <div className="col-span-3 max-sm:col-span-1 max-sm:text-center my-3">
-            <h1 className="text-xl font-bold">Nachiket Pusalkar</h1>
-            <div>
-              <p className="my-3">
-                {`Hi! My name is Nachiket and I'm a Full Stack Developer.`}{' '}
-              </p>
-              <p>{`I live in Montreal, Canada! 🇨🇦`}</p>
+      <div className="bg-vc-border-gradient rounded-lg p-px shadow-lg shadow-black/20">
+        <div className="rounded-lg bg-black p-3.5">
+          <div className="grid gap-x-4 grid-cols-4 max-sm:grid-cols-1 justify-center">
+            <div className="col-span-1 max-sm:flex max-sm:justify-center">
+              <Image
+                className="rounded-full"
+                src="/photo.webp"
+                alt="profile picture"
+                priority
+                width={160}
+                height={160}
+                style={{ width: 'auto', height: 'auto' }}
+              />
+            </div>
+            <div className="col-span-3 max-sm:col-span-1 max-sm:text-center my-3">
+              <h1 className="text-xl font-bold">Nachiket Pusalkar</h1>
+              <div>
+                <p className="my-3">
+                  {`Hi! My name is Nachiket and I'm a Full Stack Developer.`}{' '}
+                </p>
+                <p>{`I live in Montreal, Canada! 🇨🇦`}</p>
 
-              <p className="my-3">
-                {`My favourite programming languages are Ruby, Javascript, Python`}
-              </p>
+                <p className="my-3">
+                  {`My favourite programming languages are Ruby, Javascript, Python`}
+                </p>
+              </div>
             </div>
           </div>
         </div>
-      </SimpleCard>
-      <SimpleCard>
-        <h1 className="text-xl font-bold">{`What I've worked on`}</h1>
-        <Link href="/employment/vention">hi click me</Link>
-      </SimpleCard>
+      </div>
+      <div className="bg-vc-border-gradient rounded-lg p-px shadow-lg shadow-black/20">
+        <div className="rounded-lg bg-black p-3.5">
+          <div className="space-y-8">
+            <h1 className="text-xl font-medium text-gray-300">
+              {`What I've worked on`}
+            </h1>
+
+            <div className="space-y-10 text-white">
+              {demos.map((section) => {
+                return (
+                  <div key={section.name} className="space-y-5">
+                    <div className="text-xs font-semibold uppercase tracking-wider text-gray-400">
+                      {section.name}
+                    </div>
+
+                    <div className="grid grid-cols-1 gap-5 lg:grid-cols-2">
+                      {section.items.map((item) => {
+                        return (
+                          <Link
+                            href={`/${item.slug}`}
+                            key={item.name}
+                            className="group block space-y-1.5 rounded-lg bg-gray-900 px-5 py-3 hover:bg-gray-800"
+                          >
+                            <div className="font-medium text-gray-200 group-hover:text-gray-50">
+                              {item.name}
+                            </div>
+
+                            {item.description ? (
+                              <div className="line-clamp-3 text-sm text-gray-400 group-hover:text-gray-300">
+                                {item.description}
+                              </div>
+                            ) : null}
+                          </Link>
+                        );
+                      })}
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
---
+++ src/lib/demo.ts
@@ -0,0 +1,57 @@
+export type Item = {
+  name: string;
+  slug: string;
+  description?: string;
+};
+
+export const demos: { name: string; items: Item[] }[] = [
+  {
+    name: 'Employers',
+    items: [
+      {
+        name: 'Vention',
+        slug: 'employment/vention',
+        description:
+          'Ruby on Rails application for a industrial automation company involving ecommerce and industrial design.',
+      },
+      {
+        name: 'Collab Machine',
+        slug: '',
+        description:
+          'A marketplace application build on Ruby on Rails to connect freelancers with projects.',
+      },
+    ],
+  },
+  {
+    name: 'Open Source',
+    items: [
+      {
+        name: 'pay-rails/pay',
+        slug: '',
+        description: 'Payments for Ruby on Rails apps',
+      },
+      {
+        name: 'ViewComponent/view_component',
+        slug: '',
+        description:
+          'A framework for building reusable, testable & encapsulated view components in Ruby on Rails.',
+      },
+      {
+        name: 'hotwired/stimulus-rails',
+        slug: '',
+        description: 'Use Stimulus in your Ruby on Rails app',
+      },
+      {
+        name: 'nwarwick/dyte',
+        slug: '',
+        description: 'A Ruby wrapper for version 2 of the Dyte API.',
+      },
+      {
+        name: 'lcampanari/action_view_preview',
+        slug: '',
+        description:
+          "Development tool for rendering views that wouldn't otherwise be rendered in your application",
+      },
+    ],
+  },
+];
