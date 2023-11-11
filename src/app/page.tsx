28c8a5146b1897dafa3240dbfaa2f0b8a9f91a18
Nachiket Pusalkar <33430835+nachiket87@users.noreply.github.com>
2023-11-10 23:14:42 -0500

    fix component css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { demos } from '@/lib/demo';
+import SimpleCard from '@/components/simplecard';
 
 export default function Home() {
   return (
     <>
-      <div className="bg-vc-border-gradient rounded-lg p-px shadow-lg shadow-black/20">
-        <div className="rounded-lg bg-black p-3.5">
+      <div className="flex gap-x-4">
+        <SimpleCard>
           <div className="grid gap-x-4 grid-cols-4 max-sm:grid-cols-1 justify-center">
             <div className="col-span-1 max-sm:flex max-sm:justify-center">
               <Image
@@ -33,51 +34,52 @@ export default function Home() {
               </div>
             </div>
           </div>
-        </div>
+        </SimpleCard>
+        <SimpleCard>
+          <p className="my-3 h-full">Contact Information:</p>
+        </SimpleCard>
       </div>
-      <div className="bg-vc-border-gradient rounded-lg p-px shadow-lg shadow-black/20">
-        <div className="rounded-lg bg-black p-3.5">
-          <div className="space-y-8">
-            <h1 className="text-xl font-medium text-gray-300">
-              {`What I've worked on`}
-            </h1>
+      <SimpleCard>
+        <div className="space-y-8">
+          <h1 className="text-xl font-medium text-gray-300">
+            {`What I've worked on`}
+          </h1>
 
-            <div className="space-y-10 text-white">
-              {demos.map((section) => {
-                return (
-                  <div key={section.name} className="space-y-5">
-                    <div className="text-xs font-semibold uppercase tracking-wider text-gray-400">
-                      {section.name}
-                    </div>
-
-                    <div className="grid grid-cols-1 gap-5 lg:grid-cols-2">
-                      {section.items.map((item) => {
-                        return (
-                          <Link
-                            href={`/${item.slug}`}
-                            key={item.name}
-                            className="group block space-y-1.5 rounded-lg bg-gray-900 px-5 py-3 hover:bg-gray-800"
-                          >
-                            <div className="font-medium text-gray-200 group-hover:text-gray-50">
-                              {item.name}
-                            </div>
-
-                            {item.description ? (
-                              <div className="line-clamp-3 text-sm text-gray-400 group-hover:text-gray-300">
-                                {item.description}
-                              </div>
-                            ) : null}
-                          </Link>
-                        );
-                      })}
-                    </div>
+          <div className="space-y-10 text-white">
+            {demos.map((section) => {
+              return (
+                <div key={section.name} className="space-y-5">
+                  <div className="text-xs font-semibold uppercase tracking-wider text-gray-400">
+                    {section.name}
                   </div>
-                );
-              })}
-            </div>
+
+                  <div className="grid grid-cols-1 gap-5 lg:grid-cols-2">
+                    {section.items.map((item) => {
+                      return (
+                        <Link
+                          href={`/${item.slug}`}
+                          key={item.name}
+                          className="group block space-y-1.5 rounded-lg bg-gray-900 px-5 py-3 hover:bg-gray-800"
+                        >
+                          <div className="font-medium text-gray-200 group-hover:text-gray-50">
+                            {item.name}
+                          </div>
+
+                          {item.description ? (
+                            <div className="line-clamp-3 text-sm text-gray-400 group-hover:text-gray-300">
+                              {item.description}
+                            </div>
+                          ) : null}
+                        </Link>
+                      );
+                    })}
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </div>
-      </div>
+      </SimpleCard>
     </>
   );
 }
